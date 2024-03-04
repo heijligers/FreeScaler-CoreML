d9462f3dc876ec24a3165770d2895168b9b61573
B.M.R. Heijligers <bjorn@heijligers.com>
2024-03-04 22:03:23 +0100

    Updated project settings and added functionality to show custom models directory in Finder.

--- FreeScaler/AppDelegate.swift
+++ FreeScaler/AppDelegate.swift
@@ -35,16 +35,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {  }
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { return true }
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool { return true }
-
+    
     @IBAction func showCustomModelsDirInFinder(_ sender: Any) {
         let path = "FreeScaler/Models"
         if let url = URL(fileURLWithPath: path, isDirectory: true) {
-        // Implementation to show the custom models directory in Finder
-        // You will need to replace the following placeholder path with the actual path to the custom models directory
-        let path = "/path/to/custom/models/directory"
-        if let url = URL(string: path) {
-            NSWorkspace.shared.activateFileViewerSelecting([url])
+            // Implementation to show the custom models directory in Finder
+            // You will need to replace the following placeholder path with the actual path to the custom models directory
+            let path = "/path/to/custom/models/directory"
+            if let url = URL(string: path) {
+                NSWorkspace.shared.activateFileViewerSelecting([url])
+            }
         }
     }
+    
 }
-

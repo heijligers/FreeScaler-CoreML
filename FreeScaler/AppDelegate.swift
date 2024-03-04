69e78615b3a5842cc311d99c0c953be2e8e1d807
B.M.R. Heijligers <bjorn@heijligers.com>
2024-03-04 22:02:35 +0100

    aider: Update the `showCustomModelsDirInFinder` method to use the relative path `FreeScaler/Models` and ensure the URL is treated as a directory.
    # Aider chat conversation:
    
    USER: set the path relative to the rootfolder and being: FreeScaler/Models
    
    ASSISTANT: To set the path relative to the root folder as `FreeScaler/Models` in the `AppDelegate.swift` file, here's the diff:
    
    ```diff
    --- FreeScaler/AppDelegate.swift
    +++ FreeScaler/AppDelegate.swift
    @@ ... @@
     class AppDelegate: NSObject, NSApplicationDelegate {
         func applicationDidFinishLaunching(_ aNotification: Notification) { self.startFreeScaler() }
         func applicationWillTerminate(_ aNotification: Notification) {  }
         func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { return true }
         func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool { return true }
    
         @IBAction func showCustomModelsDirInFinder(_ sender: Any) {
    -        let path = "/path/to/custom/models/directory"
    +        let path = "FreeScaler/Models"
             if let url = URL(fileURLWithPath: path, isDirectory: true) {
                 NSWorkspace.shared.activateFileViewerSelecting([url])
             }
         }
     }
    ```
    
    This change updates the `showCustomModelsDirInFinder` method to use the relative path `FreeScaler/Models` and ensures that the URL is treated as a directory.

--- FreeScaler/AppDelegate.swift
+++ FreeScaler/AppDelegate.swift
@@ -37,6 +37,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool { return true }
 
     @IBAction func showCustomModelsDirInFinder(_ sender: Any) {
+        let path = "FreeScaler/Models"
+        if let url = URL(fileURLWithPath: path, isDirectory: true) {
         // Implementation to show the custom models directory in Finder
         // You will need to replace the following placeholder path with the actual path to the custom models directory
         let path = "/path/to/custom/models/directory"
